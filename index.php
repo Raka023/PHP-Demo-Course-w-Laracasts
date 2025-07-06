9d744217d8a605b33a230ba974ebec7bcb867b0c
Raka <143157921+Raka023@users.noreply.github.com>
2025-07-06 16:07:26 +0700

    using conditional statement to display a dynamic view

--- index.php
+++ index.php
@@ -4,15 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
-    
-    <h1>
-        <?php
-            $greeting = "Hello";
+    <?php
+        $name = "Andy";
+        $mature = false;
 
-            echo "$greeting Everyone!";
-        ?>
+        if ($mature) {
+            $message = "$name is old enough to watch movies";
+        } else {
+            $message = "$name isn't old enough to watch movies";
+        }
+    ?>
+
+    <h1>
+        <?= $message ?>
     </h1>
 
 </body>
