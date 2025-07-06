32503b74912961232c68f568194f8f31d7249756
Raka <143157921+Raka023@users.noreply.github.com>
2025-07-06 16:25:34 +0700

    add an array and display each of them using foreach looping

--- index.php
+++ index.php
@@ -5,30 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Demo</title>
     <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
+        * {
             font-family: sans-serif;
         }
     </style>
 </head>
 <body>
-    <?php
-        $name = "Andy";
-        $mature = false;
 
-        if ($mature) {
-            $message = "$name is old enough to watch movies";
-        } else {
-            $message = "$name isn't old enough to watch movies";
-        }
+    <h1>Peoples</h1>
+
+    <?php
+        $peoples = [
+            'Andy',
+            'Abdullah',
+            'Jojo'
+        ];
     ?>
 
-    <h1>
-        <?= $message ?>
-    </h1>
+    <ul>
+        <?php foreach($peoples as $people) : ?>
+            <li><?= $people ?></li>
+        <?php endforeach ?>
+    </ul>
 
 </body>
 </html>
