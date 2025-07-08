51cf690fed3bdfd37c7afa29127093582738b062
RaZhK_ <rakaputrautama23@outlook.com>
2025-07-08 09:59:28 +0700

    display a specific data from a get request with avoiding sql injection vulnerabilities

--- Database.php
+++ Database.php
@@ -12,9 +12,9 @@ class Database {
         $this->connection = new PDO($dsn, $username, $password, $options);
     }
 
-    public function query($query) {
+    public function query($query, $params = []) {
         $stmt = $this->connection->prepare($query);
-        $stmt->execute();
+        $stmt->execute($params);
 
         return $stmt;
     }
