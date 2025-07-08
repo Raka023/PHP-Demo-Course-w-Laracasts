abe7874fda52f007641f9b7607fdb65b515d1ed4
RaZhK_ <rakaputrautama23@outlook.com>
2025-07-09 00:35:09 +0700

    adding a server side validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -7,8 +7,25 @@ $heading = 'Notes';
 $description = "Have something's in mind?";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $currentUserId = 1;
-    $db->query("INSERT INTO notes (body, user_id) VALUES (?, ?)", [$_POST['body'], $currentUserId]);
+    $errors = [];
+
+    if (strlen($_POST['body'] <= 0)) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (strlen($_POST['body'] > 1000)) {
+        $errors['body'] = 'The body cannot be more than 1,000 characters';
+    }
+
+    if (empty($errors)) {
+        $currentUserId = 1;
+        $db->query("INSERT INTO notes (body, user_id) VALUES (?, ?)", [$_POST['body'], $currentUserId]);
+        
+        header('Location: /notes');
+        die;
+    }
+
+
 }
 
 require 'views/note-create.view.php';
